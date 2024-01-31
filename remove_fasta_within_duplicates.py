2b6ec810ced9437323cd2a9cbc8f86941f23d7d2
Madhubioinfo <mmadhubioinfo@gmail.com>
2024-01-31 14:45:51 -0500

    Add files via upload

---
+++ remove_fasta_within_duplicates.py
@@ -0,0 +1,23 @@
+from Bio import SeqIO
+
+def remove_identical_sequences(input_file, output_file):
+    sequences = {}
+    with open(input_file, 'r') as handle:
+        for record in SeqIO.parse(handle, 'fasta'):
+            sequence = str(record.seq)
+            if sequence not in sequences:
+                sequences[sequence] = record.description  # Storing the sequence ID
+
+    with open(output_file, 'w') as output_handle:
+        for sequence, seq_id in sequences.items():
+            output_handle.write(f'>{seq_id}\n')  # Writing the sequence ID
+            output_handle.write(f'{sequence}\n')  # Writing the sequence itself
+
+# Replace 'input.fasta' with the path to your input FASTA file
+input_filename = 'updated_sequences.fasta'
+
+# Replace 'output.fasta' with the desired name of the output file
+output_filename = 'duplicate_removed_stx_seq_final.fa'
+
+remove_identical_sequences(input_filename, output_filename)
+
