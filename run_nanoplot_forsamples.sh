852015aa6d36a416b5e52b11b7f2963a461ef1ca
Madhubioinfo <mmadhubioinfo@gmail.com>
2025-06-19 11:17:07 -0400

    Update run_nanoplot_forsamples.sh
    correctly adds the outputbase dir

--- run_nanoplot_forsamples.sh
+++ run_nanoplot_forsamples.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-input_dir="/mnt/nas2/raw_sequence_data/nanopore/20250522_MIN/combined_barcodes"
+input_dir="/mnt/nas2/raw_sequence_data/nanopore/20250530_MIN"
 output_base="nanoplot_reports"
 
 mkdir -p "$output_base"
 
-for fastq_file in "$input_dir"/*.fastq; do
-    sample_name=$(basename "$fastq_file" .fastq)
-    output_dir="$output_base/$sample_name"
+shopt -s nullglob  # ensures *.fastq.gz expands to empty if no matches
 
+for fastq_file in "$input_dir"/*.fastq.gz; do
+    sample_name=$(basename "$fastq_file" .fastq.gz)
+    output_dir="$output_base/$sample_name"
+    
     echo "Running NanoPlot for $sample_name"
     NanoPlot --fastq "$fastq_file" -o "$output_dir" --threads 4
 done
-
